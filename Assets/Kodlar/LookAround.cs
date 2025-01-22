647b579a44fbfa1994674f3ae4c53faa1b2a7968
Asas99 <122436587+Asas99@users.noreply.github.com>
2025-01-23 01:00:06 +0300

    aa

--- Assets/Kodlar/LookAround.cs
+++ Assets/Kodlar/LookAround.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class LookAround : MonoBehaviour
 {
     public GameObject Player;
+    public Animator AlexAnimator;
     public float Sensitivity;
 
     void Update()
@@ -15,10 +16,12 @@ public class LookAround : MonoBehaviour
     public void Look()
     {
         var x = Input.GetAxis("Mouse X");
-        var y = Input.GetAxis("Mouse Y");
 
-        if (GameObject.FindObjectOfType<Shooting>().CanShoot)
+
+        if (GameObject.FindObjectOfType<Shooting>().CanShoot && (AlexAnimator.GetBool("Has a pistol") || AlexAnimator.GetBool("Has a rifle")))
         {
+            print("qq");
+            var y = Input.GetAxis("Mouse Y");
             Camera.main.transform.eulerAngles += new Vector3(-y * Sensitivity, 0, 0);
         }
         Player.transform.eulerAngles += new Vector3(0, x * Sensitivity, 0);
