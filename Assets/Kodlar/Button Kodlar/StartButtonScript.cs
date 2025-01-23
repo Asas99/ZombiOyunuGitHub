47f72d1360acf347cce9ddf371d7a1834c55c88e
em123re <154762051+em123re@users.noreply.github.com>
2025-01-24 01:26:55 +0300

    oyun giriş UI sonrasında oyuna geçiş yapıldı

--- Assets/Kodlar/Button Kodlar/StartButtonScript.cs
+++ Assets/Kodlar/Button Kodlar/StartButtonScript.cs
@@ -9,6 +9,6 @@ public class StartButtonScript : MonoBehaviour
         Debug.Log("Start Butonuna Tıklandı!");
         
         // Yeni bir sahne yüklemek için (örneğin: "GameScene")
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("zombi 1");
     }
 }
