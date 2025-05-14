8ff1aeb3b8b5c45a7e5340d36cc40a9ee84b03bb
girebi620 <106930023+girebi620@users.noreply.github.com>
2025-05-14 10:41:16 +0300

    aa

--- Assets/UpcamProblem.cs
+++ Assets/UpcamProblem.cs
@@ -3,34 +3,17 @@ using UnityEngine;
 public class UpcamProblem : MonoBehaviour
 {
     public GameObject Alex;
-    public Vector3 normalOffset = new Vector3(0, 20, 0);  // Yakýn (minimap)
-    public Vector3 mapViewOffset = new Vector3(0, 100, 0); // Yüksek (tüm harita)
-
-    private Vector3 currentOffset;
-    private bool isMapOpen = false;
-
+    public Vector3 offset;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentOffset = normalOffset;
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Kamera konumunu güncelle
-        transform.position = Alex.transform.position + currentOffset;
+        transform.position = Alex.transform.position + offset;
 
-        // M tuþuna basýldýðýnda geniþ harita görünümü
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            isMapOpen = true;
-            currentOffset = mapViewOffset;
-        }
-
-        // ESC tuþuna basýldýðýnda normale dön
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            isMapOpen = false;
-            currentOffset = normalOffset;
-        }
     }
 }
