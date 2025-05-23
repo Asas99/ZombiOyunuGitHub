37f87b8d7b563d2cb73391c34ff417617161bff5
girebi620 <106930023+girebi620@users.noreply.github.com>
2025-05-24 01:32:40 +0300

    aa

--- Assets/ItemPickup.cs
+++ Assets/ItemPickup.cs
@@ -7,28 +7,60 @@ public class ItemPickup : MonoBehaviour
     public ItemType type;
     public int effectAmount;
 
-    void OnTriggerEnter(Collider other)
+    public Transform player;          // Player Transform'u
+    public GameObject textObject;     // Text objesi (Canvas veya 3D Text)
+    public float showDistance = 3f;   // Görünme mesafesi
+
+    private bool playerInRange = false; // Oyuncu menzilde mi?
+
+    void Update()
     {
-        if (other.CompareTag("Player"))
+        if (player == null || textObject == null)
+            return;
+
+        float distance = Vector3.Distance(player.position, transform.position);
+
+        if (distance <= showDistance)
         {
-            InventoryItem newItem = new InventoryItem
-            {
-                itemName = itemName,
-                icon = icon,
-                itemType = type,
-                restoreAmount = effectAmount
-            };
+            if (!textObject.activeSelf)
+                textObject.SetActive(true);
 
-            bool added = GameManager.Instance.AddItem(newItem);
+            playerInRange = true;
 
-            if (added)
+            // E tuşuna basıldıysa itemi alma işlemi
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                Destroy(gameObject); // Sadece envantere eklenebildiyse yok et
+                TryPickupItem();
             }
-            else
-            {
-                Debug.Log("Envanter dolu! Obje alınamadı.");
-            }
+        }
+        else
+        {
+            if (textObject.activeSelf)
+                textObject.SetActive(false);
+
+            playerInRange = false;
+        }
+    }
+
+    void TryPickupItem()
+    {
+        InventoryItem newItem = new InventoryItem
+        {
+            itemName = itemName,
+            icon = icon,
+            itemType = type,
+            restoreAmount = effectAmount
+        };
+
+        bool added = GameManager.Instance.AddItem(newItem);
+
+        if (added)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("Envanter dolu! Obje alınamadı.");
         }
     }
 }
