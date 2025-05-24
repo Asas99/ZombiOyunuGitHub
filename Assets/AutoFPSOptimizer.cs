1713433ef933e01f7b59aa39adf7ca6c2dd948b0
girebi620 <106930023+girebi620@users.noreply.github.com>
2025-05-24 19:39:59 +0300

    aaaa

---
+++ Assets/AutoFPSOptimizer.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class AutoFPSOptimizer : MonoBehaviour
+{
+    [Header("URP Asset")]
+    public UniversalRenderPipelineAsset urpAsset;
+
+    void Start()
+    {
+        OptimizeQualitySettings();
+        OptimizeURPSettings();
+        OptimizeCamera();
+    }
+
+    void OptimizeQualitySettings()
+    {
+        QualitySettings.shadowDistance = 30f;
+        QualitySettings.antiAliasing = 0;
+        QualitySettings.vSyncCount = 0;
+        QualitySettings.lodBias = 2.5f;
+
+        QualitySettings.shadows = UnityEngine.ShadowQuality.Disable;
+
+    }
+
+    void OptimizeURPSettings()
+    {
+        if (urpAsset == null)
+        {
+            Debug.LogWarning("URP Asset atanmadý!");
+            return;
+        }
+
+        urpAsset.renderScale = 0.75f;
+        urpAsset.shadowDistance = 30f;
+        urpAsset.shadowCascadeCount = 1;
+        urpAsset.msaaSampleCount = 1;
+    }
+
+    void OptimizeCamera()
+    {
+        Camera cam = Camera.main;
+        if (cam != null)
+        {
+            float[] distances = new float[32];
+            for (int i = 0; i < distances.Length; i++)
+                distances[i] = 200f;
+            cam.layerCullDistances = distances;
+        }
+    }
+}
