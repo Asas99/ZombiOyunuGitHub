1cf9472e789b53464eb41e5606acac69bb7b6644
Asas99 <122436587+Asas99@users.noreply.github.com>
2025-05-27 15:32:28 +0300

    asa

--- Assets/Kodlar/AmmoPack.cs
+++ Assets/Kodlar/AmmoPack.cs
@@ -5,16 +5,22 @@ public class AmmoPack : MonoBehaviour
 {
     public GameObject[] CompatibleWeapons;
     public int AmmoCount;
+    public Animator animator;
+    public PlayerInventory playerInventory;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GameObject.FindAnyObjectByType<CharacterController>().GetComponent<Animator>();
+        playerInventory = GameObject.FindAnyObjectByType<PlayerInventory>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            CollectAmmo();
+        }
     }
 
     public void CollectAmmo()
@@ -22,7 +28,11 @@ public class AmmoPack : MonoBehaviour
         for (int i = 0; i < gameObject.GetComponent<AmmoPack>().CompatibleWeapons.Length; i++)
         {
             gameObject.GetComponent<AmmoPack>().CompatibleWeapons[i].GetComponent<WeaponInfo>().CurrentAmmo += gameObject.GetComponent<AmmoPack>().AmmoCount;
-
         }
+        //playerInventory.ItemInfos[16].Quantity++;
+        //gameObject.GetComponent<AmmoPack>().CollectAmmo();
+        AnimatorManager.SetAllAnimatorBools(animator, "Take item");
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<MeshCollider>().enabled = false;
     }
 }
